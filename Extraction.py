f00709d011a464b7fc4f49a91bb5badeb48e277a
Freebet98 <108910779+Freebet98@users.noreply.github.com>
2023-10-12 17:21:31 -0500

    Update Extraction.py
    Added function find_divs which recursively goes through all the divs in the HTML and prints the content to the file. Edited try to call this function instead.

--- Extraction.py
+++ Extraction.py
@@ -1,8 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-#url of list of classes
+# url of list of classes
 url = "https://umtc.catalog.prod.coursedog.com/courses?page=1&cq="
+
+
+def find_divs(element):
+    if element.name == 'div':
+        text = element.text
+        file.write(text)
+        file.write("\n")
+    for child in element.find_all('div', recursive=True):
+        find_divs(child)
 
 try:
     response = requests.get(url)
@@ -10,11 +19,8 @@ try:
     if response.status_code == 200:
         file = open('output.txt', 'w')
         soup = BeautifulSoup(response.text, 'html.parser')
-        divs = soup.find_all('div')
-        for div in divs:
-            text = div.text
-            file.write(text)
-            file.write("\n")
+
+        find_divs(soup.html)
 
         file.close()
 
