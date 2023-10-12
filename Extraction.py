4b3c0c2c5e86d5f1aa66e9077a5b7782ec29f5b9
Freebet98 <108910779+Freebet98@users.noreply.github.com>
2023-10-12 18:10:11 -0500

    Update Extraction.py
    Added if to check for a button

--- Extraction.py
+++ Extraction.py
@@ -7,9 +7,10 @@ url = "https://umtc.catalog.prod.coursedog.com/courses?page=1&cq="
 
 def find_divs(element):
     if element.name == 'div':
-        text = element.text
-        file.write(text)
-        file.write("\n")
+        if element.name == 'button':
+            text = element.text
+            file.write(text)
+            file.write("\n")
     for child in element.find_all('div', recursive=True):
         find_divs(child)
 
